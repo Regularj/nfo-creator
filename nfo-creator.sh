d4467973aac1c39bb4baff3e709cf57d5302b468
Regularj <regularj@pictor.uberspace.de>
2014-06-12 08:49:59 +0200

    Update nfo-creator.sh

--- nfo-creator.sh
+++ nfo-creator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-find . -path "*[????-??-??]*" | while read folder; do
-        band_name=`echo $file_name | sed 's/ *-.*$//'` # Get rid of song name
-        band_name=`echo $band_name | sed 's/^.*\///'`  # Get rid of file path
-        song_name=`echo $file_name | sed 's/^.*- *//'` # Get rid of band name
-        song_name=`echo $song_name | sed 's/.avi//'` # Get rid of file extension
+find . -path "*[????-??-??]*" | while read folder; do # schon angepasst!
+        band_name=`echo $file_name | sed 's/ *-.*$//'` # Hier soll er anhand
+        band_name=`echo $band_name | sed 's/^.*\///'`  # der Ordnernamen die Variablen
+        song_name=`echo $file_name | sed 's/^.*- *//'` # wie Ort, Stadt, Künstler, Datum
+        song_name=`echo $song_name | sed 's/.avi//'` # vergeben. Frage ist nur, wie.
         new_file_name=`echo $file_name | sed 's/.avi/.nfo/'` # Make new filename
         echo "Making $new_file_name..."
         echo -en "<musicvideo>\n<title>$song_name</title>\n<artist>$band_name</artist>\n</musicvideo>\n" > "$new_file_name"
